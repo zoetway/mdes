2c811a7f69692805237972f90cdfe59c75a438f4
zoetway <rob@load81.net>
2016-04-26 10:47:03 +0200

    Add mic input with alsaaudio an audioop, TEST!

--- simplecv.py
+++ simplecv.py
@@ -4,8 +4,12 @@ from SimpleCV import Camera
 import operator
 import requests
 import datetime
+import alsaaudio #import as per http://stackoverflow.com/questions/1936828/how-get-sound-input-from-microphone-in-python-and-process-it-on-the-fly
+import audioop, time #maybe remove these later, example imports for audio
 
 cam = Camera()
+
+#Define header for JSON API Request
 headers = dict()
 headers['Ocp-Apim-Subscription-Key']= 'empty'
 headers['Content-Type']='application/octet-stream'
@@ -13,7 +17,18 @@ url = "https://api.projectoxford.ai/emotion/v1.0/recognize"
 maxNumberRetries = 10
 json = None
 imagePath = '/home/pi/Pictures/'
+
+#Read M$ API Key from file
 pathToKeyFile = '/home/pi/keys.txt'
+
+#Create audio input device
+inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NONBLOCK) 
+
+# Set audio attributes: Mono, 8000 Hz, 16 bit little endian samples
+inp.setchannels(1)
+inp.setrate(8000)
+inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
+inp.setperiodsize(160)
 
 def processRequest(json, data, headers):
     retries = 0
@@ -43,6 +58,7 @@ headers['Ocp-Apim-Subscription-Key']=file.readline()
 
 while True:
     print("Searching for faces...")
+    print("And listening for sounds now too!")
     
     img = cam.getImage()
     faces = img.findHaarFeatures('/home/pi/PythonProjects/haarcascade_frontalface_alt.xml')                           
@@ -64,3 +80,9 @@ while True:
                     currentEmotion = max(currentFace['scores'].iteritems(), key=operator.itemgetter(1))[0]
                 print(result)
                 print('Microsoft thinks your current emotion is: ' + currentEmotion)
+    
+    #start checking for audio on mic
+    l,audioData = inp.read()
+    if l:
+        print audioop.max(data,2)
+    
